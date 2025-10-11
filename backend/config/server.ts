f3c0215128bcc6dbdb20b48fdda63d249525a3f3
Morad17 <morad123@hotmail.co.uk>
2025-10-11 17:35:49 +0100

    added postgresql

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 10000),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  url: env("RENDER_EXTERNAL_URL") || env("PUBLIC_URL"),
 });
