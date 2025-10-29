63b58b7a781a989feb640801415e209573d865ed
Morad17 <morad123@hotmail.co.uk>
2025-10-29 10:03:45 +0000

    changeing backend deploy to railway

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,31 +1,16 @@
 import path from "path";
 
-export default ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    // Parse the DATABASE_URL
-    const databaseUrl = env("DATABASE_URL");
-
-    return {
-      connection: {
-        client: "postgres",
-        connection: databaseUrl,
-        acquireConnectionTimeout: 60000,
-        pool: {
-          min: 2,
-          max: 10,
-        },
-      },
-    };
-  }
-
-  // Development (SQLite)
-  return {
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "sqlite",
-      connection: {
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-      },
-      useNullAsDefault: true,
+      host: env("PGHOST"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE"),
+      user: env("PGUSER"),
+      password: env("PGPASSWORD"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
-  };
-};
+    debug: false,
+  },
+});
